e230ec75bf7d91cded2614274a3879158f0586c2
Omkar Bansod <omkarbansod221@gmail.com>
2024-09-10 16:47:48 +0530

    Update src directory

---
+++ src/constants/index.ts
@@ -0,0 +1,145 @@
+import { Icons } from "@/components";
+
+export const perks = [
+    {
+        icon: Icons.auth,
+        title: "Integrate",
+        info: "Integrate your LLm agent with IDE and initialize project. ",
+    },
+    {
+        icon: Icons.customize,
+        title: "Build",
+        info: "Pick a Problem and start building solution with your LLM agent.",
+    },
+    {
+        icon: Icons.launch,
+        title: "Launch",
+        info: "Publish your Product and share it with the world.",
+    },
+];
+
+export const features = [
+    {
+        icon: Icons.brain,
+        title: "The Latest LLMs at your Fingertip",
+        info: "Leverage your choice of cloud, custom, and local LLMs based on a selection of the most advanced AI models.",
+    },
+    {
+        icon: Icons.context,
+        title: "Context for your Conversations",
+        info: "Make better decisions by applying hyper-relevant context from your work-in-progress journey to your copilot chats.",
+    },
+    {
+        icon: Icons.workflow,
+        title: "Embedded Across your Workflow",
+        info: "Pick up where you left off with deep integrations within your browser, IDE, and productivity tools.",
+    },
+    {
+        icon: Icons.secure,
+        title: "Air-Gapped Security and Privacy",
+        info: "our LLm stores and processes data 100% offline, keeping your code on-device even while using robust AI features.",
+    },
+    {
+        icon: Icons.Ai,
+        title: "Get AI-based suggestions in real time",
+        info: "Random Ai Copilot suggests code completions as developers type and turns natural language prompts into coding .",
+    },
+    {
+        icon: Icons.predict,
+        title: "Smart Prediction",
+        info: "LLm agent predicts your next cursor position so you can seamlessly navigate your code.",
+    },
+];
+
+export const pricingCards = [
+    {
+        title: "Starter",
+        description: "Perfect for trying out plura",
+        price: "Free",
+        duration: "",
+        highlight: "Key features",
+        buttonText: "Start for free",
+        features: ["Pro two-week trial", "2000 completions", "50 slow premium requests"],
+        priceId: "",
+    },
+    {
+        title: "Pro",
+        description: "Everything in Hobby, plus",
+        price: "$20/Month",
+        duration: "month",
+        highlight: "Key features",
+        buttonText: "Upgrade to Pro",
+        features: ["Unlimited completions", "500 fast premium requests per month", "Unlimited slow premium requests", "Unlimited cursor-small uses"],
+        priceId: "price_1OYxkqFj9oKEERu1KfJGWxgN",
+    },
+    {
+        title: "Enterprise",
+        description: "Everything in Pro, plus",
+        price: "$40/User/Month",
+        duration: "month",
+        highlight: "Everything in Starter, plus",
+        buttonText: "Upgrade to Enterprise",
+        features: ["Unlimited completions", "Centralized billing", "Admin usage dashboard", "OpenAI/Anthropic zero-data retention", "Priority support (24/7)"],
+        priceId: "price_1OYxkqFj9oKEERu1NbKUxXxN",
+    },
+];
+
+export const bentoCards = [
+    {
+        title: 'Start with Inspiration',
+        info: 'Browse our vast library of pre-designed templates or upload your own images.',
+        imgSrc: '/assets/bento-1.svg', // Lightbulb or Inspiration icon
+        alt: 'Inspiration for website creation'
+    },
+    {
+        title: 'AI Assists Your Vision',
+        info: 'Our intelligent AI tailors suggestions and functionalities based on your goals.',
+        imgSrc: '/assets/bento1.svg', // AI Assistant icon
+        alt: 'AI website building assistant'
+    },
+    {
+        title: 'Drag & Drop Customization',
+        info: 'Effortlessly personalize your website with our intuitive drag-and-drop editor.',
+        imgSrc: '/assets/bento1.svg', // Drag and Drop icon or hand editing a website
+        alt: 'Website customization with drag and drop'
+    },
+    {
+        title: 'Launch & Shine Online',
+        info: 'Publish your website with a single click and take your brand to the world.',
+        imgSrc: '/assets/bento1.svg', // Rocket launching or website going live
+        alt: 'Website launch and publication'
+    },
+];
+
+export const reviews = [
+    {
+        name: "Jack",
+        username: "@jack",
+        body: "I've never seen anything like this before. It's amazing. I love it.",
+    },
+    {
+        name: "Jill",
+        username: "@jill",
+        body: "I don't know what to say. I'm speechless. This is amazing.",
+    },
+    {
+        name: "John",
+        username: "@john",
+        body: "I'm at a loss for words. This is amazing. I love it.",
+    },
+    {
+        name: "Jane",
+        username: "@jane",
+        body: "I'm at a loss for words. This is amazing. I love it.",
+    },
+    {
+        name: "Jenny",
+        username: "@jenny",
+        body: "I'm at a loss for words. This is amazing. I love it.",
+    },
+    {
+        name: "James",
+        username: "@james",
+        body: "I'm at a loss for words. This is amazing. I love it.",
+    },
+];
